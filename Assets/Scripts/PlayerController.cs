fd907f166b320e645078cc5392bf23bcebb960e0
Paulo Victor <paulovictor.joao@gmail.com>
2017-10-03 11:14:41 -0300

    Mudando de Player e adicionando a movimentacao basica na horizontal

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float horizontalSpeed = 10f;
+
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1 = esquerda, 1 = direita,
+		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+		if (horizontalPlayerSpeed != 0){
+			MoveHorizontal(horizontalPlayerSpeed);
+		}
+		else{
+			StopMovingHorizontal();
+		}
+	}
+
+	void MoveHorizontal(float speed){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+	}
+
+	void StopMovingHorizontal(){
+		rb.velocity = new Vector2(0f, rb.velocity.y);
+	}
+}
