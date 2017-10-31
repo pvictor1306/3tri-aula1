b19fcdc829e02ccd309b9ef29e86a03679b84fa9
Paulo Victor <paulovictor.joao@gmail.com>
2017-10-31 10:41:58 -0200

    Retirando o duplo pulo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,13 +22,26 @@ public class PlayerController : MonoBehaviour {
 
 	bool isJumping = false;
 
+	public Transform feet;
+	public float feetWidth = 0.5f;
+	public float feetHeight = 0.1f;
+	public bool isGrounded;
+	public LayerMask whatIsGround;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
 	}
+
+	void OnDrawGizmos (){
+		Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeight, 0f));
+	}
 	
 	void Update () {
+
+		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
+
 		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1 = esquerda, 1 = direita,
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
 		if (horizontalPlayerSpeed != 0){
@@ -44,6 +57,8 @@ public class PlayerController : MonoBehaviour {
 
 		ShowFalling();
 	}
+
+	
 
 	void MoveHorizontal(float speed){
 		rb.velocity = new Vector2(speed, rb.velocity.y);
@@ -75,10 +90,13 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Jump(){
+		if (isGrounded){
+
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 
 		anim.SetInteger("State", 1);
+	}
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
